9b8231f8844901022ebb7903e6921d464d8b475f
j2hyun <j2hyun0911@gmail.com>
2024-10-30 18:18:48 +0900

    Committing changes before deploy

---
+++ layouts/default.vue
@@ -0,0 +1,47 @@
+<template>
+  <div :class="`ly-wrap ${dark ? 'dark-mode' : 'light-mode'}`">
+    <header class="ly-header">
+      <div class="ly-switch">
+        <div class="txt-caption">Switch Theme</div>
+        <div class="switch">
+          <input type="checkbox" id="switch" /><label
+            for="switch"
+            @click="switchTheme"
+            >Switch Toggle</label
+          >
+        </div>
+      </div>
+    </header>
+    <div class="ly-inner">
+      <div class="ly-container">
+        <nuxt />
+      </div>
+      <button class="btn-top" @click="scrollTop">
+        <i class="icon">☝️</i>
+      </button>
+    </div>
+    <footer class="ly-footer">
+      <div class="footer">&copy; All Rights Reserved By. Ji Hyun Kim</div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dark: false,
+    };
+  },
+  methods: {
+    switchTheme() {
+      this.dark = !this.dark;
+    },
+    scrollTop() {
+      window.scrollTo(0, 0);
+    },
+  },
+};
+</script>
+
+<style></style>
